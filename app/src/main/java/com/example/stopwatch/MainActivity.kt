4ee98e1b33a57580744c63628dd10920ce16e540
gururani-abhishek <gururani.abhishek@gmail.com>
2023-04-12 23:06:18 +0530

    added stopwatch nickname

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -3,20 +3,40 @@ package com.example.stopwatch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
-import android.widget.Button
-import android.widget.Chronometer
+import android.provider.ContactsContract.CommonDataKinds.Nickname
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var stopwatch : Chronometer // holds reference to Chronometer
     private var running = false // flag for stopwatch running/stop
     var offset : Long = 0 // will hold the correct time in case of pause, reset.
+    lateinit var tvStopwatchNickname : TextView
+    //keys for Bundle
+    val OFFSET_KEY = "offset"
+    val BASE_KEY = "base"
+    val RUNNING_KEY = "running"
+    val NICKNAME_KEY = "nickname"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // get ref to stopwatch
         stopwatch = findViewById(R.id.stopwatch)
+        tvStopwatchNickname = findViewById(R.id.tv_stopwatch_nickname)
+        if(savedInstanceState != null) {
+            tvStopwatchNickname.text = savedInstanceState.getString(NICKNAME_KEY)
+            // the activity is being re-instantiated, and there must be a saved bundle
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if(running) {
+                // if it has been running, you want it to run
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            } else {
+                setBaseTime()
+            }
+        }
 
         // start button starts the watch if it's not running
         val startButton = findViewById<Button>(R.id.startBTN)
@@ -50,6 +70,25 @@ class MainActivity : AppCompatActivity() {
             setBaseTime()
             stopwatch.stop()
         }
+
+        val generateNicknameButton = findViewById<Button>(R.id.bt_generate_nickname)
+        generateNicknameButton.setOnClickListener {
+            val etNickname = findViewById<EditText>(R.id.et_nickname)
+            val etNicknameText = etNickname.text.toString()
+            if(etNicknameText.isNotEmpty()) {
+                tvStopwatchNickname.text = etNicknameText
+            }
+        }
+
+    }
+
+    // called right before onDestroy()
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putString(NICKNAME_KEY, tvStopwatchNickname.text.toString())
+        savedInstanceState.putBoolean(RUNNING_KEY, running)    // a bundle takes input as key-value pair
+        savedInstanceState.putLong(OFFSET_KEY, offset)
+        savedInstanceState.putLong(BASE_KEY, stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
     }
 
     private fun setBaseTime() {
