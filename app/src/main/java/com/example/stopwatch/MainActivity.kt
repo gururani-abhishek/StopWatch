fc2308b66d8306125a088936743eb8c6c29326ed
gururani-abhishek <gururani.abhishek@gmail.com>
2023-04-16 15:40:26 +0530

    added viewBindings

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -3,15 +3,16 @@ package com.example.stopwatch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
-import android.provider.ContactsContract.CommonDataKinds.Nickname
 import android.widget.*
+import com.example.stopwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var stopwatch : Chronometer // holds reference to Chronometer
+    private lateinit var binding : ActivityMainBinding // Binding object for activity_main.xml layout
+//    private lateinit var stopwatch : Chronometer // holds reference to Chronometer
     private var running = false // flag for stopwatch running/stop
     var offset : Long = 0 // will hold the correct time in case of pause, reset.
-    lateinit var tvStopwatchNickname : TextView
+//    lateinit var tvStopwatchNickname : TextView
     //keys for Bundle
     val OFFSET_KEY = "offset"
     val BASE_KEY = "base"
@@ -19,65 +20,68 @@ class MainActivity : AppCompatActivity() {
     val NICKNAME_KEY = "nickname"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) // links activity_main.xml to binding object
+        val rootView = binding.root
+        setContentView(rootView)
 
         // get ref to stopwatch
-        stopwatch = findViewById(R.id.stopwatch)
-        tvStopwatchNickname = findViewById(R.id.tv_stopwatch_nickname)
+//        stopwatch = findViewById(R.id.stopwatch)
+//        tvStopwatchNickname = findViewById(R.id.tv_stopwatch_nickname)
+//        tvStopwatchNickname = binding.tvStopwatchNickname
         if(savedInstanceState != null) {
 
-            tvStopwatchNickname.text = savedInstanceState.getString(NICKNAME_KEY)
+            binding.tvStopwatchNickname.text = savedInstanceState.getString(NICKNAME_KEY)
             // the activity is being re-instantiated, and there must be a saved bundle
             offset = savedInstanceState.getLong(OFFSET_KEY)
             running = savedInstanceState.getBoolean(RUNNING_KEY)
             if(running) {
                 // if it has been running, you want it to run
-                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
-                stopwatch.start()
+                binding.stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                binding.stopwatch.start()
             } else {
                 setBaseTime()
             }
         }
 
         // start button starts the watch if it's not running
-        val startButton = findViewById<Button>(R.id.startBTN)
-        startButton.setOnClickListener {
+//        val startButton = findViewById<Button>(R.id.startBTN)
+        binding.startBTN.setOnClickListener {
             if(!running) {
                 setBaseTime() // to set the base property of chronometer
-                stopwatch.start() // will start from whatever base-time you set
+                binding.stopwatch.start() // will start from whatever base-time you set
                 running = true
             }
         }
 
         // pause button will pause the stopwatch
-        val pauseButton = findViewById<Button>(R.id.pauseBTN)
-        pauseButton.setOnClickListener {
+//        val pauseButton = findViewById<Button>(R.id.pauseBTN)
+        binding.pauseBTN.setOnClickListener {
             saveOffset() // whenever interrupted save time as offset
-            stopwatch.stop()
+            binding.stopwatch.stop()
             running = false
         }
 
         // to reset the timer
-        val resetButton = findViewById<Button>(R.id.resetBTN)
-        resetButton.setOnClickListener {
+//        val resetButton = findViewById<Button>(R.id.resetBTN)
+        binding.resetBTN.setOnClickListener {
             offset = 0
             setBaseTime()
         }
 
-        val stopButton = findViewById<Button>(R.id.stopBTN)
-        stopButton.setOnClickListener {
+//        val stopButton = findViewById<Button>(R.id.stopBTN)
+        binding.stopBTN.setOnClickListener {
             offset = 0
             running = false
             setBaseTime()
-            stopwatch.stop()
+            binding.stopwatch.stop()
         }
 
-        val generateNicknameButton = findViewById<Button>(R.id.bt_generate_nickname)
-        generateNicknameButton.setOnClickListener {
+//        val generateNicknameButton = findViewById<Button>(R.id.bt_generate_nickname)
+        binding.btGenerateNickname.setOnClickListener {
             val etNickname = findViewById<EditText>(R.id.et_nickname)
             val etNicknameText = etNickname.text.toString()
             if(etNicknameText.isNotEmpty()) {
-                tvStopwatchNickname.text = etNicknameText
+                binding.tvStopwatchNickname.text = etNicknameText
             }
         }
 
@@ -85,10 +89,10 @@ class MainActivity : AppCompatActivity() {
 
     // called right before onDestroy()
     override fun onSaveInstanceState(savedInstanceState: Bundle) {
-        savedInstanceState.putString(NICKNAME_KEY, tvStopwatchNickname.text.toString())
+        savedInstanceState.putString(NICKNAME_KEY, binding.tvStopwatchNickname.text.toString())
         savedInstanceState.putBoolean(RUNNING_KEY, running)    // a bundle takes input as key-value pair
         savedInstanceState.putLong(OFFSET_KEY, offset)
-        savedInstanceState.putLong(BASE_KEY, stopwatch.base)
+        savedInstanceState.putLong(BASE_KEY, binding.stopwatch.base)
         super.onSaveInstanceState(savedInstanceState)
     }
 
@@ -97,7 +101,7 @@ class MainActivity : AppCompatActivity() {
         // stopwatch should pause(if was running before)
         if(running) {
             saveOffset()
-            stopwatch.stop()
+            binding.stopwatch.stop()
         }
     }
 
@@ -107,15 +111,15 @@ class MainActivity : AppCompatActivity() {
         if(running) {
             setBaseTime()
             offset = 0
-            stopwatch.start()
+            binding.stopwatch.start()
         }
     }
 
     private fun setBaseTime() {
-        stopwatch.base = SystemClock.elapsedRealtime() - offset
+        binding.stopwatch.base = SystemClock.elapsedRealtime() - offset
     }
 
     private fun saveOffset() {
-        offset = SystemClock.elapsedRealtime() - stopwatch.base
+        offset = SystemClock.elapsedRealtime() - binding.stopwatch.base
     }
 }
